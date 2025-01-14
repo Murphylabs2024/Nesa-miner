ba0c1d6fed9852e20b24a3c41fbd304f5273e685
Murphylabs2024 <murphynodeteam@gmail.com>
2025-01-14 16:22:00 +0700

    Update nesaSetup.sh

--- code/nesaSetup.sh
+++ code/nesaSetup.sh
@@ -21,7 +21,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
+sudo apt-get update 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Example command to install curl on Ubuntu
@@ -40,5 +40,7 @@ sudo ufw status
 # Allow necessary ports (example for port 26656)
 sudo ufw allow 26656
 
-bash <(curl -s https://raw.githubusercontent.com/nesaorg/bootstrap/master/bootstrap.sh) -y
+curl -s https://raw.githubusercontent.com/nesaorg/bootstrap/master/bootstrap.sh -y
+chmod +x bootstrap.sh
+bash bootstrap.sh
 
